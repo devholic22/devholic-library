ed033c8e55b403db77d8202a0bc31a8cc69633f2
devholic22 <hyunjoon.tech@gmail.com>
2023-09-01 15:47:33 +0900

    [feat] 구글 OAuth 기능 개발

---
+++ src/main/java/devholic/library/google/TokenAgent.java
@@ -0,0 +1,47 @@
+package devholic.library.google;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TokenAgent {
+
+    private final String RESOURCE_URL = "https://www.googleapis.com/oauth2/v2/userinfo";
+
+    public Map<String, String> getUserResource(String accessToken) throws IOException {
+        URL url = new URL(RESOURCE_URL);
+
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("Authorization", "Bearer " + accessToken);
+
+        int responseCode = connection.getResponseCode();
+        if (responseCode != 200) {
+            throw new IOException("Failed to get user resource: " + responseCode);
+        }
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        Map<String, String> info = new HashMap<>();
+        while (true) {
+            String row = br.readLine();
+            if (row == null)
+                break;
+            String[] each;
+            if (row.startsWith("name")) {
+                each = row.split(":");
+            } else {
+                each = row.split(" ");
+            }
+            if (each.length <= 2)
+                continue;
+            String key = each[2];
+            String value = each[3];
+            info.put(key, value);
+        }
+        return info;
+    }
+}
